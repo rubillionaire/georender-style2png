948044b7b4e5668756253dd01b20b3743672ea0d
rubonics <rubenrodriguez+github@gmail.com>
2024-12-09 12:02:52 -0400

    [example] patch: migrate from `example/make-tex` to `example/make-assets` because we add a little label config for `mixmap-georender` to run off of, specifically for defining the order of names of fonts that are stored in the tex.

---
+++ example/make-assets.js
@@ -0,0 +1,23 @@
+const fs = require('node:fs')
+const makePNG = require('fast-png')
+const makeTex = require('../')
+makeTex({
+  stylesheet: require('./style.json'),
+  features: require('@rubenrodriguez/georender-pack/features.json'),
+  defaults: require('../defaults.json')
+}, function (error, data) {
+  if (error) return console.log(error)
+  const pngData = {
+    width: data.width,
+    height: data.height,
+    data: data.data
+  }
+  var png = makePNG.encode(pngData)
+  fs.writeFileSync('style-texture.png', png)
+  const labelOpts = {
+    label: {
+      fontFamily: data.labelFontFamily,
+    }
+  }
+  fs.writeFileSync('style-label-opts.json', JSON.stringify(labelOpts))
+})
